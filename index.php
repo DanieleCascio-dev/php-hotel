15683caa2a0e57e5d5e1a500c2b5bd2e800e816a
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-12-12 16:12:16 +0100

    add: table

--- index.php
+++ index.php
@@ -41,11 +41,52 @@
     ];
 
 
-    foreach ($hotels as $hotel){
-      // var_dump($hotel);
-      foreach ($hotel as $key => $value) {
-        echo $key . " " . $value . "<br>";
-      }
-    }
+    // foreach ($hotels as $hotel){
+    //   // var_dump($hotel);
+    //   foreach ($hotel as $key => $value) {
+    //     echo $key . " " . $value . "<br>";
+    //   }
+    // }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Custom Style -->
+  <link rel="stylesheet" href="style.css">
+  <title>Hotels</title>
+</head>
+<body>
+
+<h2>Hotels</h2>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to center</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($hotels as $hotel){
+      ?>
+    <tr>
+      <th scope="row"><?php echo $hotel['name'] ?></th>
+      <td><?php echo $hotel['description'] ?></td>
+      <td><?php echo $hotel['parking'] ?></td>
+      <td><?php echo $hotel['vote'] ?></td>
+      <td><?php echo $hotel['distance_to_center'] ?></td>
+    </tr> 
+    <?php } ?>
+  </tbody>
+</table>
+  
+</body>
+</html>
